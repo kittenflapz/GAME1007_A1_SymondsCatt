4cdbe85d0ad87e476fb52bfdc5d8cb3e3490d764
Catt Symonds <catherine.symonds.thompson@gmail.com>
2021-01-17 17:40:11 -0500

    set up game modes and resource gathering modes

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public enum GameState {
+    START,
+    RESOURCE_GATHERING
+}
+
+public enum ResourceGatheringMode
+{
+    SCAN,
+    EXTRACT
+}
+
+public class GameManager : MonoBehaviour
+{
+    public GameState gameState;
+    public ResourceGatheringMode resourceGatheringMode;
+
+    // todo: do this with tags instead of manually dragging everything in like a caveman
+    public List<GameObject> startStateObjects;
+    public List<GameObject> resourceGatheringObjects;
+
+    // resource gathering mode stuff
+    public TextMeshProUGUI currentModeText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // initialize to the start scene
+        gameState = GameState.START;
+        foreach (GameObject obj in startStateObjects)
+        {
+            obj.SetActive(true);
+        }
+        foreach (GameObject obj in resourceGatheringObjects)
+        {
+            obj.SetActive(false);
+        }
+
+        // initialize the game itself to start in extract mode
+        resourceGatheringMode = ResourceGatheringMode.EXTRACT;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    // kinda lomg
+    public void ToggleGameState()
+    {
+        if (gameState == GameState.RESOURCE_GATHERING)
+        {
+            gameState = GameState.START;
+            foreach (GameObject obj in startStateObjects)
+            {
+                obj.SetActive(true);
+            }
+            foreach (GameObject obj in resourceGatheringObjects)
+            {
+                obj.SetActive(false);
+            }
+        }
+        else
+        {
+            gameState = GameState.RESOURCE_GATHERING;
+            foreach (GameObject obj in startStateObjects)
+            {
+                obj.SetActive(false);
+            }
+            foreach (GameObject obj in resourceGatheringObjects)
+            {
+                obj.SetActive(true);
+            }
+        }
+    }
+
+
+    public void ToggleResourceGatheringMode()
+    {
+        if (resourceGatheringMode == ResourceGatheringMode.SCAN)
+        {
+            resourceGatheringMode = ResourceGatheringMode.EXTRACT;
+            currentModeText.SetText("Extract");
+        }
+        else
+        {
+            resourceGatheringMode = ResourceGatheringMode.SCAN;
+            currentModeText.SetText("Scan");
+        }
+    }
+}
